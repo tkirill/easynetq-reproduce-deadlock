dcc50e7aa76cf47abb7a7607a67c396df552ce1a
t_kirill <t_kirill@skbkontur.ru>
2016-10-27 15:36:40 +0500

    Add a code to reproduce hijacking of Client Command Dispatcher Thread.
    The algorithm is:
    
    * Subscribe the consumer via `SubscribeAsync`
    * Publish 10k messages in the main thread
    * The consumer sends three another messages via `await PublishAsync`
    * Main thread sleeps for a minute giving EasyNetQ time for consuming the messages
    * Usually within 15 seconds one of the `PublishAsync` from the consumer will be executed on the Client Command Dispatcher Thread and processing of new messages will be freezed
    
    Obviosly one of the continuations of `await PublishAsync` inside the consumer was executed synchronously.

--- ReproduceThreadSteale/Program.cs
+++ ReproduceThreadSteale/Program.cs
@@ -1,9 +1,62 @@
-﻿namespace ReproduceThreadSteale
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using EasyNetQ;
+
+namespace ReproduceThreadSteale
 {
     class Program
     {
+        private const int NumberOfInitialMessages = 10000;
+
+        private static IBus bus;
+        private static StreamWriter output;
+        private static Random random;
+
         static void Main(string[] args)
         {
+            output = File.CreateText("log.txt");
+            bus = RabbitHutch.CreateBus("host=localhost");
+            random = new Random();
+            using (output)
+            using (bus)
+            {
+                bus.SubscribeAsync<TestMessage>("consumer", Consume);
+
+                for (int i = 0; i < NumberOfInitialMessages; i++)
+                    bus.Publish(new TestMessage {Number = random.Next(1000)});
+
+                Thread.Sleep(TimeSpan.FromSeconds(60));
+            }
         }
+
+        static async Task Consume(TestMessage msg)
+        {
+            Console.WriteLine($"Consume {msg.Number}");
+            for (int i = 0; i < 10; i++)
+            {
+                await bus.PublishAsync(new TestMessage {Number = random.Next(1000)});
+                await CheckThread();
+                await bus.PublishAsync(new TestMessage { Number = random.Next(1000) });
+                await CheckThread();
+                await bus.PublishAsync(new TestMessage { Number = random.Next(1000) });
+                await CheckThread();
+            }
+        }
+
+        private static async Task CheckThread()
+        {
+            if (!string.IsNullOrWhiteSpace(Thread.CurrentThread.Name))
+            {
+                Console.WriteLine(Thread.CurrentThread.Name);
+                await output.WriteLineAsync(Thread.CurrentThread.Name);
+            }
+        }
+    }
+
+    class TestMessage
+    {
+        public int Number { get; set; }
     }
 }
